6faaa9d7cac7b27077d4bd8677880f6bef3043d6
Raymond <raymond.hoang@water.ca.gov>
2025-11-18 13:24:38 -0800

    add new vars for debug

--- study_loader.py
+++ study_loader.py
@@ -19,9 +19,13 @@ date_map = pd.read_csv("config/date_map.csv", index_col=0, parse_dates=True)
 
 studies = [
 
-    Study(r"C:\dev\calsim3-dcr\DSS\output\DCR2025_DV_9.8.1_Danube_Adj_v4.0_QWEST.dss",
-          r"C:\jobs\20230428_DCR23\models\9.3.1_danube_adj\DSS\input\DCR2023_SV_Danube_Adj_v1.8.dss",
-          "DCR25_AdjHist_QWEST", "QWEST", "Current", 1)
+    Study(r"C:\jobs\20251020_Alt5\_models\DCR_9.8.0_SDR_Off\DSS\output\DCR2025_DV_9.8.0_Danube_Adj_v4.0_OMRbase2.dss",
+          r"C:\jobs\20251020_Alt5\_models\DCR_9.8.0_SDR_Off\DSS\input\DCR2025_SV_Danube_Adj_v4.0.dss",
+          "DCR25_AdjHist_noSDR", "DCR25", "Current", 1)
+    Study(r"C:\jobs\20251020_Alt5\_models\Alt5_1.3.2\DSS\output\Alt5_1.3.2_adjHist.dss",
+          r"C:\jobs\20251020_Alt5\_models\Alt5_1.3.2\DSS\input\DCR2025_SV_Danube_Adj_v4.0.dss",
+          "Alt5_1.3.2", "Alt5", "Current", 1)
+
 
 ]
 
